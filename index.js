376d6e19a8cb506934ec4d2722e60a3ce9436972
Taanveer22 <taanveer469@gmail.com>
2026-01-10 15:43:25 +0600

    alhamdulliah basic server setup done with mongo

---
+++ index.js
@@ -0,0 +1,62 @@
+// ==================Recommended Order=================
+// 1. Required by common js (express, cors, etc.)
+// 2 .App Initialization (const app = express())
+// 3. Middleware Setup (cors, json, logging)
+// 4. Database Configuration & Connection (MongoDB client setup and run() function)
+// 5. Routes
+// 6. Server Startup (app.listen)
+// simple-crud
+// KdPFhFNtgrMKubiG
+// =======================================================
+
+// === 1. required ===
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// === 2. instance initialization ===
+const app = express();
+const port = process.env.PORT || 5000;
+
+// === 3. middleware setup ====
+app.use(express.json());
+app.use(cors());
+
+// === 4. mongodb setup ===
+const uri =
+  "mongodb+srv://simple-crud:KdPFhFNtgrMKubiG@cluster0.89rnkti.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+run();
+
+// === 5.routes setup ===
+app.get("/", (req, res) => {
+  res.send("the server is working");
+});
+
+// === 6. server startup ===
+app.listen(port, () => {
+  console.log(`server running on port : ${port}`);
+});
