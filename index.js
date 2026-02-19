1925758f3301c40860ca01365116d8de44fb98df
Taanveer22 <taanveer469@gmail.com>
2026-02-19 07:32:52 +0600

    alhamdulliah dot env file added

--- index.js
+++ index.js
@@ -5,11 +5,10 @@
 // 4. Database Configuration & Connection (MongoDB client setup and run() function)
 // 5. Routes
 // 6. Server Startup (app.listen)
-// simple-crud
-// KdPFhFNtgrMKubiG
 // =======================================================
 
 // === 1. required ===
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -23,8 +22,8 @@ app.use(express.json());
 app.use(cors());
 
 // === 4. mongodb setup ===
-const uri =
-  "mongodb+srv://simple-crud:KdPFhFNtgrMKubiG@cluster0.89rnkti.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.89rnkti.mongodb.net/?appName=Cluster0`;
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -42,7 +41,7 @@ async function run() {
 
     const myDb = client.db("studentDb");
     const myColl = myDb.collection("students");
-    
+
     // ====received data from client side=========
     // === send data to mongodb ==============
     // =======by these methods ============
@@ -99,7 +98,7 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
+      "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } catch (error) {
     console.log(error);
